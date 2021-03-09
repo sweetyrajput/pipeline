525813a5250f5219cf1b98c60ca7b0cd550b2ce0
Sweety <sweety.sweety@ibm.com>
2021-03-09 13:39:50 +0530

    demo

---
+++ addition/Addition.java
@@ -0,0 +1,21 @@
+package com.addition;
+
+public class Addition {
+	public static void main(String args[]) {
+		
+	}
+		int a;
+		int b;
+		int result;
+		void add(int a, int b) {
+			
+
+
+		result=a+b;
+		
+		System.out.println("result is:" +result);
+		
+
+
+		}
+		}
