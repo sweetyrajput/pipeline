f808c15476fd3015e7bf43b7964f716848aeb8a9
Sweety <Sweety@LAPTOP-C1KRFCSL>
2021-03-10 00:15:24 +0530

    dfdfdd

--- addition/Addition.java
+++ addition/Addition.java
@@ -1,5 +1,7 @@
 package com.addition;
 
+
+
 public class Addition {
 	public static void main(String args[]) {
 		
@@ -8,14 +10,11 @@ public class Addition {
 		int b;
 		int result;
 		void add(int a, int b) {
-			
-
-
 		result=a+b;
-		
 		System.out.println("result is:" +result);
 		
 
 
 		}
 		}
+
